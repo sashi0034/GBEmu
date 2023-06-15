ad9c361b663e5d7efc4650e65d92c9cf0961427f
sashi0034 <satoshi3104.nishi@gmail.com>
2023-06-15 21:35:08 +0900

    magic_enumで128以上を表示できるように修正

--- GBEmu/stdafx.h
+++ GBEmu/stdafx.h
@@ -1,5 +1,9 @@
 ﻿#pragma once
 //# define NO_S3D_USING
+
+#define MAGIC_ENUM_RANGE_MIN (-15)
+#define MAGIC_ENUM_RANGE_MAX 255
+
 #include <Siv3D.hpp>
 #include "Include/magic_enum/magic_enum.hpp"
 #include "Include/nameof/nameof.hpp"
