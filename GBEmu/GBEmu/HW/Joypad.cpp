b51e12ae7eedc688fc5783fc72a42cf1614b13a8
sashi0034 <satoshi3104.nishi@gmail.com>
2023-06-17 22:34:23 +0900

    Joypadのバグを修正

--- GBEmu/GBEmu/HW/Joypad.cpp
+++ GBEmu/GBEmu/HW/Joypad.cpp
@@ -14,12 +14,13 @@ namespace GBEmu::HW
 
 	void Joypad::UpdateFrame(Memory& memory)
 	{
-		Update(memory, m_controlBefore);
+		// TODO: 除く?
+		// Update(memory, m_controlBefore);
 	}
 
 	uint8 Joypad::fetchInput(uint8 control)
 	{
-		if (control & (1 << 4))
+		if ((control & (1 << 4)) == 0)
 		{
 			return 0b11100000 |
 				(KeyRight.pressed() || KeyD.pressed() ? 0 : 1 << 0) |
@@ -27,17 +28,17 @@ namespace GBEmu::HW
 				(KeyUp.pressed() || KeyW.pressed() ? 0 : 1 << 2) |
 				(KeyDown.pressed() || KeyS.pressed() ? 0 : 1 << 3);
 		}
-		else if (control & (1 << 5))
+		else if ((control & (1 << 5)) == 0)
 		{
 			return 0b11010000 |
 				(KeySlash.pressed() || KeyZ.pressed() ? 0 : 1 << 0) | // A
-				(KeyBackspace.pressed() || KeyX.pressed() ? 0 : 1 << 1) | // B
+				(KeyUnderscore_JIS.pressed() || KeyX.pressed() ? 0 : 1 << 1) | // B
 				(KeySpace.pressed() ? 0 : 1 << 2) | // Select
 				(KeyEnter.pressed() ? 0 : 1 << 3); // Start
 		}
 		else
 		{
-			return 0;
+			return 0b11010000;
 		}
 	}
 }
