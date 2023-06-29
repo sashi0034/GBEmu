d27b68000572085e088a2c1729bc11ce22648e02
sashi0034 <satoshi3104.nishi@gmail.com>
2023-06-29 12:19:35 +0900

    Rangeを修正

--- GBEmu/GBEmu/Util/Range.h
+++ GBEmu/GBEmu/Util/Range.h
@@ -5,12 +5,12 @@ namespace GBEmu::Util
 	template <typename T>
 	class Range
 	{
-		static_assert(std::is_arithmetic_v<T>, "Template argument must be an arithmetic type (int, double, float, etc.).");
+		static_assert(std::is_arithmetic_v<T>, "template argument must be an arithmetic type");
 	private:
 		const T m_Min;
 		const T m_Max;
 	public:
-		Range(T minInclusive, T maxInclusive) :
+		constexpr Range(T minInclusive, T maxInclusive) :
 			m_Min(minInclusive), m_Max(maxInclusive)
 		{}
 
@@ -29,29 +29,28 @@ namespace GBEmu::Util
 		T Max() const {return m_Max; }
 		T Min() const {return m_Min; }
 
-		Range<T> ExtendRange(T extension)
+		constexpr Range<T> ExtendRange(T extension)
 		{
 			if (extension < 0) return WithSort(m_Min - extension, m_Max + extension);
 			return Range(m_Min - extension, m_Max + extension);
 		}
 
-		T GetDiff() const{
+		constexpr T GetDiff() const{
 			return m_Max - m_Min;
 		}
 
-		bool IsBetween(T targetValue) const
+		constexpr bool IsBetween(T targetValue) const
 		{
 			return m_Min <= targetValue && targetValue <= m_Max;
 		}
 
-		T Clamp(T targetValue) const
+		constexpr T Clamp(T targetValue) const
 		{
 			return (std::max)(m_Min, (std::min)(targetValue, m_Max));
 		}
 
 		T Normalize(T targetValue, const Range<T>&afterRange)
 		{
-			assert(this->IsBetween(targetValue));
 			T normalizedRate = (targetValue - this->m_Min) / (this->m_Max - this->m_Min);
 			T result = afterRange.m_Min + (afterRange.m_Max - afterRange.m_Min) * normalizedRate;
 			return result;
